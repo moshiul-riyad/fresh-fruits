d978ae0e3e5b950d4432c67277adfc6467e497ef
moshiul-riyad <moshiulriyad@gmail.com>
2023-01-14 10:40:08 -0600

    add display product dynamically

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ class Products {
         try {
             let result = await fetch("products.json");
             let data = await result.json();
-            return data;
+            let products = data.items;
+            products = products.map(item =>{
+                const {title,price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image}
+            })
+            return products;
         } catch (error) {
             console.log(error);
         }
@@ -27,18 +34,32 @@ class Products {
 
 // display products
 class UI {
+    displayProducts(products){
+        let result = '';
+        products.forEach(product => {
+            result += `<article class="product">
+            <div class="img-container">
+                <img src=${product.image} alt="product" class="product-img">
+                <button class="bag-btn" data-id=${product.id}>
+                    <i class="fas fa-shopping-cart">Add to Bag</i>
+                </button>
+            </div>
 
+            <h3>${product.title}</h3>
+            <h4>$${product.price}</h4>
+        </article>`
+        })
+    }
 }
 
 // local storage
-class Storage {
-
-}
+class Storage {}
 
 document.addEventListener("DOMContentLoaded", ()=> {
     const ui = new UI();
     const products = new Products();
+    // get all products
+    products.getProducts().then(products => ui.displayProducts(products));
 })
 
-// getting all products
-products.getProducts().then(data => console.log(data));
+
